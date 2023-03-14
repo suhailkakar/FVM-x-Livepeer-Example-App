eecc23bc901ef7846a4388f3df9577f13fe13e31
Suhail Kakar <suhail.zahed@gmail.com>
2023-03-14 11:21:13 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,10 +4,6 @@ require("dotenv").config();
 module.exports = {
   solidity: "0.8.9",
   networks: {
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.PRIVATE_KEY],
-    },
   },
   paths: {
     artifacts: "./artifacts",
